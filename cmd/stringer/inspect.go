b9217aedb5664eac1a152febf7a167b93f9c9d03
ahy69195 <ahy69195@uga.edu>
2023-03-13 23:07:49 -0400

    inspect and reverse code added

---
+++ cmd/stringer/inspect.go
@@ -0,0 +1,30 @@
+package stringer
+
+import (
+	"fmt"
+
+	"github.com/ahy69195/CLITutorial2/pkg/stringer"
+	"github.com/spf13/cobra"
+)
+
+var inspectCmd = &cobra.Command{
+	Use:     "inspect",
+	Aliases: []string{"insp"},
+	Short:   "Inspects a string",
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		i := args[0]
+		res, kind := stringer.Inspect(i, false)
+
+		pluralS := "s"
+		if res == 1 {
+			pluralS = ""
+		}
+		fmt.Printf("'%s' has a %d %s%s.\n", i, res, kind, pluralS)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(inspectCmd)
+}
